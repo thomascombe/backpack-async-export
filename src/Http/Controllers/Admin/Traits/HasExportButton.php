f4ab21b7426b80edcf2e2a773af0a751a54cae19
thomascombe <thomascombe42@gmail.com>
2021-04-21 18:16:13 +0200

    Check interface implementation

--- src/Http/Controllers/Admin/Traits/HasExportButton.php
+++ src/Http/Controllers/Admin/Traits/HasExportButton.php
@@ -4,17 +4,27 @@ namespace Thomascombe\BackpackAsyncExport\Http\Controllers\Admin\Traits;
 
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Route;
+use Thomascombe\BackpackAsyncExport\Http\Controllers\Admin\Interfaces\ExportableCrud;
 use Thomascombe\BackpackAsyncExport\Jobs\ExportJob;
 
 trait HasExportButton
 {
+    /**
+     * @throws \Exception
+     */
     protected function addExportButtons()
     {
+        $this->checkInterfaceImplementation();
         $this->crud->addButton('top', 'export', 'view', 'backpack-async-export::buttons/export', 'end');
     }
 
+    /**
+     * @throws \Exception
+     */
     protected function setupExportRoutes($segment, $routeName, $controller)
     {
+        $this->checkInterfaceImplementation();
+
         Route::get($segment . '/' . config('backpack-async-export.admin_route'), [
             'as' => $routeName.'.export',
             'uses' => $controller.'@export',
@@ -22,13 +32,28 @@ trait HasExportButton
         ]);
     }
 
+    /**
+     * @throws \Exception
+     */
     public function export(): RedirectResponse
     {
+        $this->checkInterfaceImplementation();
+
         list($export, $parameters) = $this->getExport();
 
         ExportJob::dispatch($export, $parameters);
         \Alert::info(__('backpack-async-export::export.notifications.queued'))->flash();
 
         return response()->redirectToRoute('export.index');
+    }
+
+    /**
+     * @throws \Exception
+     */
+    protected function checkInterfaceImplementation(): void
+    {
+        if (!$this instanceof ExportableCrud) {
+            throw new \Exception(sprintf('%s need to implement %s', self::class, ExportableCrud::class));
+        }
     }
 }

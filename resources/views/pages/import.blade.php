b9ab816d30148fb8173cb5c37aacec718ddd3532
Jérémy ARGOUD <jargoud@webqam.fr>
2022-09-06 14:23:38 +0200

    feat(crud): update import view for Backpack 5

--- resources/views/pages/import.blade.php
+++ resources/views/pages/import.blade.php
@@ -1,61 +1,60 @@
-@extends('backpack::layouts.top_left')
+@extends(backpack_view('blank'))
 
 @section('header')
-    <section class="content-header">
-        <h1>
+    <section class="container-fluid">
+        <h2>
             <span class="text-capitalize">{!! $crud->getHeading() ?? $crud->entity_name_plural !!}</span>
-            <small>{!! $crud->getSubheading() ?? trans('backpack-async-export::admin.operation.import') . ' ' . $crud->entity_name !!}.</small>
-        </h1>
-        @include('backpack::inc.breadcrumbs')
+            <small>{!! $crud->getSubheading() ?? trans('backpack-async-export::admin.operation.import') . ' ' . $crud->entity_name !!}
+                .</small>
+
+            @if($crud->hasAccess('list'))
+                <small>
+                    <a href="{{ url($crud->route) }}" class="d-print-none font-sm">
+                        <i class="la la-angle-double-{{ config('backpack.base.html_direction') == 'rtl' ? 'right' : 'left' }}"></i>
+                        @lang('backpack::crud.back_to_all')
+                        <span>{{ $crud->entity_name_plural }}</span>
+                    </a>
+                </small>
+            @endif
+        </h2>
     </section>
 @endsection
 
 @section('content')
-    @if ($crud->hasAccess('list'))
-        <a href="{{ url($crud->route) }}" class="hidden-print">
-            <span class="fa fa-angle-double-left"></span>
-            {{ trans('backpack::crud.back_to_all') }}
-            <span>{{ $crud->entity_name_plural }}</span>
-        </a>
-    @endif
-
-    <div class="row m-t-20">
+    <div class="row">
         <div class="{{ $crud->getEditContentClass() }}">
-            <!-- Default box -->
-
             @include('crud::inc.grouped_errors')
 
-            <form method="post" action="{{ url($crud->route . '/import') }}" enctype="multipart/form-data">
+            <form action="{{ url($crud->route . '/import') }}" enctype="multipart/form-data" method="post">
                 @csrf
 
-                <div class="col-md-12">
-                    <div class="">
-                        <!-- load the view from the application if it exists, otherwise load the one in the package -->
-                        @if(view()->exists('backpack-async-export::vendor.backpack.crud.form_content'))
-                            @include('backpack-async-export::vendor.backpack.crud.form_content', ['fields' => $fields, 'action' => 'edit'])
-                        @elseif(view()->exists('vendor.backpack.crud.form_content'))
-                            @include('vendor.backpack.crud.form_content', ['fields' => $fields, 'action' => 'edit'])
-                        @else
-                            @include('crud::form_content', ['fields' => $fields, 'action' => 'edit'])
-                        @endif
+                @includeFirst(
+                  [
+                    'backpack-async-export::vendor.backpack.crud.form_content',
+                    'vendor.backpack.crud.form_content',
+                    'crud::form_content'
+                  ],
+                  [
+                    'fields' => $fields,
+                    'action' => 'edit',
+                  ]
+                )
 
+                <div class="d-none" id="parentLoadedAssets">{{ json_encode(Assets::loaded()) }}</div>
+                <div id="saveActions" class="form-group">
+                    <div class="btn-group" role="group">
+                        <button type="submit" class="btn btn-success">
+                            <span class="la la-save" role="presentation" aria-hidden="true"></span> &nbsp;
+                            <span
+                                data-value="@lang('backpack-async-export::admin.button.import')">@lang('backpack-async-export::admin.button.import')</span>
+                        </button>
                     </div>
 
-                    <div class="form-group">
-                        <div class="btn-group">
-                            <button type="submit" class="btn btn-success">
-                                <span class="fa fa-upload" role="presentation" aria-hidden="true"></span>
-                                <span
-                                    data-value="{{ trans('backpack-async-export::admin.button.import') }}">{{ trans('backpack-async-export::admin.button.import') }}</span>
-                            </button>
-                        </div>
-
-                        <a href="{{ $crud->hasAccess('list') ? url($crud->route) : url()->previous() }}"
-                           class="btn btn-default">
-                            <span class="fa fa-ban"></span>
-                            {{ trans('backpack::crud.cancel') }}
+                    @if(!$crud->hasOperationSetting('showCancelButton') || $crud->getOperationSetting('showCancelButton') == true)
+                        <a href="{{ $crud->hasAccess('list') ? url($crud->route) : url()->previous() }}" class="btn btn-default">
+                            <span class="la la-ban"></span> &nbsp;@lang('backpack::crud.cancel')
                         </a>
-                    </div>
+                    @endif
                 </div>
             </form>
         </div>

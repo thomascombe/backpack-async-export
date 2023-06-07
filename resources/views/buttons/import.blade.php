a14f829983cd7d45f479a8baffe376968236f5ad
thomascombe <thomascombe42@gmail.com>
2023-06-07 17:32:30 +0200

    fix: import full URL

--- resources/views/buttons/import.blade.php
+++ resources/views/buttons/import.blade.php
@@ -1,6 +1,6 @@
 @if ($crud->hasAccess('list'))
     @php($route = $crud->get('import_route'))
-    @php($url = url($crud->route . '/' . $route))
+    @php($url = url('/' . $route))
 
     <a href="{{ $url }}" class="btn btn-secondary">
         <i class="la la-upload"></i> @lang('backpack-async-export::import.buttons.import')
